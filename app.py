fc60687973d92449f1cef737f1bcd8c192a2d977
Nireshkumar R <nireshkmr.raj@gmail.com>
2019-09-01 02:48:15 +0530

    tester

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 from flask_cors import CORS
 import pickle
+from sklearn import preprocessing
 
 app = Flask(__name__)
 CORS(app)
@@ -29,7 +30,7 @@ def test():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    with open('model.obj','rb') as fp:
+    with open('d_model.obj','rb') as fp:
         model = pickle.load(fp)
         data = request.get_json()
         to_extract= ['Gender', 'family_history', 'work_interfere', 'remote_work',
@@ -43,9 +44,25 @@ def predict():
             input.append(data[i])
             keys.append(i)
         
-        df = pd.DataFrame([input],columns=keys)
+        test = pd.DataFrame([input],columns=keys)
 
+        # temp = test.Age
+        # temp = temp.replace(to_replace=-1,value=22)
+        # test.Age = temp
 
+        test['work_interfere']=test['work_interfere'].fillna("Maybe")
+        # test['self_employed']=test['self_employed'].fillna("Dont Know")
 
-        result = model.predict(input)
-        return str(result)
+        temp = test.Gender
+        temp = temp.replace(to_replace=["Male","male","m","Malr","Male ","Cis Man"],value='M')
+        temp = temp.replace(to_replace=["female","F","Female","Woman","femail","f"],value='F')
+        temp = temp.replace(to_replace=["p","Female (trans)","ostensibly male, unsure what that really means"],value='T')
+        test.Gender = temp
+
+        le = preprocessing.LabelEncoder()
+        test = test.apply(le.fit_transform) 
+        
+
+        result = model.predict(test)
+        result = 'Yes' if result==0 else 'No'
+        return {"data":result}
