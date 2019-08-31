c2a6109b661b1995497b1b75888479a2e38e7e3d
Nireshkumar R <nireshkmr.raj@gmail.com>
2019-09-01 02:14:14 +0530

    committed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import request
 import json
 import pandas as pd
 from flask_cors import CORS
-
+import pickle
 
 app = Flask(__name__)
 CORS(app)
@@ -11,9 +11,31 @@ CORS(app)
 @app.route('/userdata')
 def fetchdata():
     df = pd.read_csv('testms.csv') 
-    data = df.to_json()
+    data = []
+    keys = df.columns
+    for i,j in df.iterrows():
+        item = {}
+        for index,val in enumerate(j):
+            item[keys[index]]=val
+        data.append(item)
     return json.dumps({"data":data})
 
 @app.route('/test')
 def test():
     return "hello"
+
+@app.route('/predict')
+def predict():
+    with open('model.obj','rb') as fp:
+        model = pickle.load(fp)
+        data = request.get_json()
+        to_extract= ['Gender', 'family_history', 'work_interfere', 'remote_work',
+       'tech_company', 'benefits', 'care_options', 'wellness_program',
+       'seek_help', 'mental_health_consequence', 'phys_health_consequence',
+       'mental_health_interview', 'phys_health_interview',
+       'mental_vs_physical', 'obs_consequence']
+        input=[]
+        for i in to_extract:
+            input.append(data[i])
+        result = model.predict(input)
+        return str(result)
